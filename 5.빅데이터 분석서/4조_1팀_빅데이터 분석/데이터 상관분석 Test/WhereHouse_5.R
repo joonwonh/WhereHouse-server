c8dee10aabc2bbd1a014869766ddc6bf81f702b8
JWHan <96328871+joonwonh@users.noreply.github.com>
2024-01-16 10:48:06 +0900

    docs: 빅데이터 분석서 업로드

---
+++ 5.빅데이터 분석서/4조_1팀_빅데이터 분석/데이터 상관분석 Test/WhereHouse_5.R
@@ -0,0 +1,258 @@
+library('tidyverse')
+library(MASS)
+
+#인구밀집도 데이터 구 별
+setwd("C:/Users/admin/Desktop/R_Data/인구밀집도");
+population_density <- read.csv("인구밀집도.csv", header = T);
+
+#지하철
+setwd("C:/Users/admin/Desktop/R_Data/지하철");
+subway <- read.csv("지하철.csv", header = T);
+
+#음식점
+setwd("C:/Users/admin/Desktop/R_Data/지하철");
+subway <- read.csv("지하철.csv", header = T);
+
+#올리브영
+setwd("C:/Users/admin/Desktop/R_Data/올리브영");
+olive <- read.csv("올리브영.csv", header=T);
+
+#다이소
+setwd("C:/Users/admin/Desktop/R_Data/다이소");
+daiso <- read.csv("다이소.csv", header=T);
+
+#대규모점포
+setwd("C:/Users/admin/Desktop/R_Data/대규모점포");
+shop <- read.csv("대규모점포.csv", header=T);
+
+#영화관 데이터
+setwd("C:/Users/admin/Desktop/R_Data/영화관");
+cinema <- read.csv("영화관.csv", header=T);
+
+#공원
+setwd("C:/Users/admin/Desktop/R_Data/공원");
+park <- read.csv("공원.csv", header=T);
+
+total <- data.frame(gu_name=subway$gu_name
+                    , conv_store=rep(0,25)
+                    , cafe=rep(0,25)
+                    , subway=subway$subway_cnt
+                    , restaurant=rep(0,25)
+                    , daiso=daiso$daiso_cnt
+                    , olive=olive$olive_cnt
+                    , shop=shop$shop_cnt
+                    , cinema=rep(0,25)
+                    , park=rep(0,25));
+
+#편의시설
+for(i in 1:nrow(total)){
+  setwd(paste("C:/Users/admin/Desktop/R_Data/편의시설/",total[i,"gu_name"],sep = ""));
+  conv_store <- read.csv(paste(total[i,"gu_name"],"_편의시설.csv", sep = ""), header=T);
+  print(i);
+  for(j in 1:nrow(conv_store)){
+    if(conv_store[j,"code"] == "G47122"){
+      total[i,"conv_store"] <- total[i,"conv_store"] + 1;
+    } else if(conv_store[j,"code"] == "I56221"){
+      total[i,"cafe"] <- total[i,"cafe"] + 1;
+    }
+  }
+}
+sum(total$conv_store)
+
+#영화관
+for(i in 1:nrow(cinema)){
+  for(j in 1:nrow(total)){
+    if(str_detect(cinema[i,"도로명주소"],total[j,"gu_name"])){
+      total[j,"cinema"] <- total[j,"cinema"] + 1;
+    }
+  }
+}
+
+#음식점
+setwd("C:/Users/admin/Desktop/R_Data/음식점");
+restaurant <- read.csv("음식점.csv", header=T);
+for(i in 1:nrow(restaurant)){
+  if(i%%10000 == 0){
+    print(i);
+  }
+  for(j in 1:nrow(total)){
+    if(str_detect(restaurant[i,"지번주소"],total[j,"gu_name"])){
+      total[j,"restaurant"] <- total[j,"restaurant"] + 1;
+    }
+  }
+}
+
+#공원
+for(i in 1:nrow(park)){
+  for(j in 1:nrow(total)){
+    if(total[j,"gu_name"]==park[i,"지역"]){
+      total[j,"park"] <- total[j,"park"] + 1;
+    }
+  }
+}
+
+total$conv_store / population_density$area;
+
+setTotal <- function(){
+  total.result <- total
+  for(i in 1:nrow(total)){
+    #편의점
+    total.result[i,"conv_store"] <- total[i,"conv_store"] * 0.1818 - 18.18
+    if(total.result[i,"conv_store"] > 100){
+      total.result[i,"conv_store"] <- 100;
+    }
+    total.result[i,"conv_store"] <- total.result[i,"conv_store"] * 0.16;
+    
+    #카페
+    total.result[i,"cafe"] <- total[i,"cafe"] * 0.1111 - 22.22
+    if(total.result[i,"cafe"] > 100){
+      total.result[i,"cafe"] <- 100;
+    }
+    total.result[i,"cafe"] <- total.result[i,"cafe"] * 0.14;
+    
+    #지하철
+    total.result[i,"subway"] <- total[i,"subway"] * 5 - 5
+    if(total.result[i,"subway"] > 100){
+      total.result[i,"subway"] <- 100;
+    }
+    total.result[i,"subway"] <- total.result[i,"subway"] * 0.13;
+    
+    #음식점
+    total.result[i,"restaurant"] <- total[i,"restaurant"] * 0.0125 - 12.5
+    if(total.result[i,"restaurant"] > 100){
+      total.result[i,"restaurant"] <- 100;
+    }
+    total.result[i,"restaurant"] <- total.result[i,"restaurant"] * 0.12;
+    
+    #다이소
+    total.result[i,"daiso"] <- total[i,"daiso"] * 6.6666 - 6.6666
+    if(total.result[i,"daiso"] > 100){
+      total.result[i,"daiso"] <- 100;
+    }
+    total.result[i,"daiso"] <- total.result[i,"daiso"] * 0.11;
+    
+    #올리브영
+    total.result[i,"olive"] <- total[i,"olive"] * 3.3333 - 3.3333
+    if(total.result[i,"olive"] > 100){
+      total.result[i,"olive"] <- 100;
+    }
+    total.result[i,"olive"] <- total.result[i,"olive"] * 0.1;
+    
+    #쇼핑몰
+    total.result[i,"shop"] <- total[i,"shop"] * 2.2222 - 11.111
+    if(total.result[i,"shop"] > 100){
+      total.result[i,"shop"] <- 100;
+    }
+    total.result[i,"shop"] <- total.result[i,"shop"] * 0.09;
+    
+    #영화관
+    total.result[i,"cinema"] <- total[i,"cinema"] * 10 - 0
+    if(total.result[i,"cinema"] > 100){
+      total.result[i,"cinema"] <- 100;
+    }
+    total.result[i,"cinema"] <- total.result[i,"cinema"] * 0.08;
+    
+    #공원
+    total.result[i,"park"] <- total[i,"park"] * 10 - 10
+    if(total.result[i,"park"] > 100){
+      total.result[i,"park"] <- 100;
+    }
+    total.result[i,"park"] <- total.result[i,"park"] * 0.07;
+    
+    #총합점수
+    total.result[i,"score"] <- (total.result[i,"conv_store"]
+                                + total.result[i,"cafe"]
+                                + total.result[i,"subway"]
+                                + total.result[i,"restaurant"]
+                                + total.result[i,"daiso"]
+                                + total.result[i,"olive"]
+                                + total.result[i,"shop"]
+                                + total.result[i,"cinema"] + 5);
+  }
+  #total.order <- order(total.result$score);
+  #total.order
+  
+  for(i in 1:nrow(total.result)){
+    if(total.result[i,"score"]<=30){
+      total.result[i,"index"] <- 1;
+    } else if(total.result[i,"score"]<=35){
+      total.result[i,"index"] <- 2;
+    } else if(total.result[i,"score"]<=45){
+      total.result[i,"index"] <- 3;
+    } else if(total.result[i,"score"]<=50){
+      total.result[i,"index"] <- 4;
+    } else if(total.result[i,"score"]<=55){
+      total.result[i,"index"] <- 5;
+    } else if(total.result[i,"score"]<=66){
+      total.result[i,"index"] <- 6;
+    } else if(total.result[i,"score"]<=79){
+      total.result[i,"index"] <- 7;
+    } else if(total.result[i,"score"]<=90){
+      total.result[i,"index"] <- 8;
+    } else {
+      total.result[i,"index"] <- 9;
+    }
+    
+  }
+  total.result$index <- factor(total.result$index);
+  return (total.result);
+}
+total.result$score;
+
+min(total.result$score);
+max(total.result$score);
+
+mean(total.result$score);
+sort(total.result$score);
+total.result <- setTotal();
+
+total.result
+
+
+#library(ggplot2)
+color <- c("lightcoral","salmon","coral","orangered","tomato","indianred","red","firebrick", "darkred");
+ggplot(total.result, aes(x=gu_name, y=index)) +
+  geom_bar(stat="identity",
+           width=0.7,
+           fill="steelblue");
+
+str(total.result);
+total.result$index <- as.integer(total.result$index);
+ggplot(total.result, aes(x=index)) +
+  geom_histogram(binwidth = 1,position = "dodge");
+
+total.result$index
+
+str(total.result)
+
+total.result;
+new.total.result <- total.result;
+new.total.result$index <- as.integer(total.result$index);
+total.result$index
+model.total <- glm(index~conv_store 
+                   + cafe 
+                   + subway
+                   + restaurant
+                   + daiso
+                   + olive
+                   + shop
+                   + cinema
+                   + park
+                   + score
+                   ,data = new.total.result);
+
+summary(model.total)
+anova(model.total)
+total.test <- total.result;
+
+#정확도
+pred <- predict(model.total, total.test);
+pred <- round(pred, 0);
+pred
+new.total.result$index
+
+answer <- as.integer(total.result$index);
+pred == answer;
+acc <- mean(pred == answer);
+acc
+
